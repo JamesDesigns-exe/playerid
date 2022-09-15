e15491a83ae30a8d96f5c7c548476e37590d3fc5
JamesDesigns-exe <113489236+JamesDesigns-exe@users.noreply.github.com>
2022-09-14 19:14:06 -0600

    Update playeridscurrent.lua

--- playeridscurrent.lua
+++ playeridscurrent.lua
@@ -1,3 +1,15 @@
+string1 = function(GetPlayerServerId)
 RegisterCommand('serverId', function()
     print(GetPlayerServerId(PlayerId()))
 end, false)
+
+
+
+RegisterCommand('Players', function()
+    print(GetPlayerServerId(PlayerId()))
+end, false)
+
+RegisterCommand('table', function()
+    print(string1value, string1 )
+end, false)
+end
