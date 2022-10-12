6ad539a97a81e6c358dae755f24f7ee07fe0b67f
JamesDesigns-exe <113489236+JamesDesigns-exe@users.noreply.github.com>
2022-10-12 14:58:15 -0600

    update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,6 +7,6 @@ version '1.0.0'
 
 -- What to run
 client_scripts {
-    'playeridscurrent.lua',
+    'client.lua',
 }
 --server_script 'server.lua'
---
+++ serverconfig.lua
@@ -0,0 +1,35 @@
+--server config
+string1 = 'table'
+    Createfunction.function('run = string1value', function())
+        print(function'string1value', function
+    end)
+
+RequestModel(`adder`)
+
+
+if getentitymodel(vehicle) = 'adder' then
+    print('indeed its a 'adder')
+end
+RegisterCommand('table', function()
+    if true return function()
+    print('table')
+    end)
+RegisterCommand('getentitymodel', function()
+    print(function = '')
+    end
+
+-------------------------------------------------------------below to test la
+
+
+Citizen.CreateThread(function()
+    while true do 
+          citizen.wait(1000)
+    end
+end)
+addeventhandler('testeventforcallback'function(testingforcallback))
+    print(('I just received %s from %i'):format(text,source))
+end)
+
+
+
+    TriggerServerEvent('testeventforcallback', 'TESTINGCALLBACK')
